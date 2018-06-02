b6f5a9e1946b169d049d6df8fa22d45f5b9690de
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-02 16:09:20 -0500

    Initial commit of Deck data structure

---
+++ src/js/game-objects/deck/deck.js
@@ -0,0 +1,75 @@
+/**
+ * Class to represent a deck of cards (represented as IDs). Each card can in one of three places: in
+ * the deck, in discard or in play.
+ *
+ * @export
+ * @class Deck
+ */
+export default class Deck {
+  /**
+   * @param {Object[]} composition An array of objects that describes the composition of the types
+   * of cards in the deck, in the form { id: 0, quantity: 10 }
+   */
+  constructor(composition) {
+    this.composition = composition;
+
+    this.cardsInDeck = [];
+    this.cardsInDiscard = [];
+    this.cardsInPlay = [];
+
+    // Generate the initial shuffled deck
+    composition.map(({ id, quantity }) => {
+      this.cardsInDeck = this.cardsInDeck.concat(new Array(quantity).fill(id));
+    });
+    Phaser.Utils.Array.Shuffle(this.cardsInDeck);
+
+    this.totalCardsInDeck = this.cardsInDeck.length;
+  }
+
+  /**
+   * Draw a card from the deck and registers it as in play.
+   *
+   * @returns {*|null} The ID of the card from the deck that was just drawn, or null if no cards
+   * remain.
+   * @memberof Deck
+   */
+  draw() {
+    if (this.cardsInDeck.length === 0) return null;
+    else {
+      const card = this.cardsInDeck.pop();
+      this.cardsInPlay.push(card);
+      return card;
+    }
+  }
+
+  /**
+   * Discard the given card, if it is found in the cards that are currently in play.
+   *
+   * @param {*} card The ID of the card to discard.
+   * @memberof Deck
+   */
+  discard(card) {
+    const index = this.cardsInPlay.indexOf(card);
+    if (index !== -1) {
+      this.cardsInPlay = [
+        ...this.cardsInPlay.slice(0, index),
+        ...this.cardsInPlay.slice(index + 1)
+      ];
+      this.cardsInDiscard.push(card);
+    } else {
+      console.warn("Attempting to discard a card that is not in the deck!");
+    }
+  }
+
+  getNumCardsRemaining() {
+    return this.cardsInDeck.length;
+  }
+
+  getNumCardsDiscarded() {
+    return this.cardsInDiscard.length;
+  }
+
+  getNumCardsInDeck() {
+    return this.totalCardsInDeck;
+  }
+}
--- src/js/scenes/play-scene.js
+++ src/js/scenes/play-scene.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import DraggableCard from "../game-objects/draggable-card";
+import Deck from "../game-objects/deck/deck";
 
 export default class PlayScene extends Phaser.Scene {
   create() {
@@ -10,6 +11,20 @@ export default class PlayScene extends Phaser.Scene {
     new DraggableCard(this, 100, 100);
     new DraggableCard(this, 300, 100);
     new DraggableCard(this, 500, 100);
+
+    const ENEMY_CARD_TYPES = {
+      WEAK_ENEMY: 0,
+      STRONG_ENEMY: 1,
+      BLANK: 2
+    };
+
+    const composition = [
+      { id: ENEMY_CARD_TYPES.WEAK_ENEMY, quantity: 17 },
+      { id: ENEMY_CARD_TYPES.STRONG_ENEMY, quantity: 3 },
+      { id: ENEMY_CARD_TYPES.BLANK, quantity: 30 }
+    ];
+
+    const deck = new Deck(composition);
   }
 
   update(time, delta) {}
