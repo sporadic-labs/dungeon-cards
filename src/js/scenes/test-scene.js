75508d3b5e6e280a1f2afdf653884d1135800269
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-02 08:35:52 -0500

    Remove test code from sandbox repo

--- src/js/scenes/test-scene.js
+++ src/js/scenes/test-scene.js
@@ -8,15 +8,8 @@ export default class Scene extends Phaser.Scene {
       .tileSprite(0, 0, 750, 750, "assets", "subtle-pattern-ep-natural-black")
       .setOrigin(0, 0);
 
-    this.rotatingGameObject = new GameObjectExtendingLifecycle(this, 100, 300, "assets", "ship");
-
-    new Player(this, 50, 50);
   }
 
   update(time, delta) {
-    if (time > 5000 && this.rotatingGameObject) {
-      this.rotatingGameObject.destroy();
-      this.rotatingGameObject = null;
-    }
   }
 }
