18b0b6c2d2220d4f3eb3dde2389a1d6ac925fbf9
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-03 09:12:07 -0500

    Shift over draggable card test instances

--- src/js/scenes/play-scene.js
+++ src/js/scenes/play-scene.js
@@ -14,9 +14,9 @@ export default class PlayScene extends Scene {
       .tileSprite(0, 0, width, height, "assets", "subtle-pattern-ep-natural-black")
       .setOrigin(0, 0);
 
-    new DraggableCard(this, 100, 100, "cards/blank");
-    new DraggableCard(this, 300, 100, "cards/weak-enemy");
-    new DraggableCard(this, 500, 100, "cards/strong-enemy");
+    new DraggableCard(this, 600, 100, "cards/blank");
+    new DraggableCard(this, 700, 100, "cards/weak-enemy");
+    new DraggableCard(this, 800, 100, "cards/strong-enemy");
 
     this.board = new GameBoard(this, 4, 5);
 
