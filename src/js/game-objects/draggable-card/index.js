9e87f2881d17d48bd1df7eceae2a307ec0e05667
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-03 08:36:41 -0500

    Try new card asset in DraggableCard

--- src/js/game-objects/draggable-card/index.js
+++ src/js/game-objects/draggable-card/index.js
@@ -11,14 +11,13 @@ export default class DraggableCard extends LifecycleObject {
     super(scene);
 
     this.sprite = scene.add
-      .sprite(x, y, "assets", "cardBack_blue5")
-      .setTint(0x8fdaff)
+      .sprite(x, y, "assets", "cards/strong-enemy")
       .setDepth(3)
       .setInteractive(); // Enables pointer events but not drag
 
     scene.input.setDraggable(this.sprite);
-    this.sprite.on("pointerover", () => this.sprite.setTint(0xffffff));
-    this.sprite.on("pointerout", () => this.sprite.setTint(0x8fdaff));
+    this.sprite.on("pointerover", () => this.sprite.setTint(0x8fdaff));
+    this.sprite.on("pointerout", () => this.sprite.setTint(0xffffff));
     this.sprite.on("dragstart", this.onDragStart);
     this.sprite.on("drag", this.onDrag);
     this.sprite.on("dragend", this.onDragEnd);
