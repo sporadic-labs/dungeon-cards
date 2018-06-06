1d937e35c70055d73a09a4031c33dd0e1b028709
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-05 22:09:41 -0500

    Add (temporary) ability to click on card to select it

--- src/js/game-objects/player-card/player-card.js
+++ src/js/game-objects/player-card/player-card.js
@@ -7,20 +7,72 @@ export default class PlayerCard {
    */
   constructor(scene, type, x, y) {
     this.type = type;
+    this.scene = scene;
 
     // TODO: this is just a simple wrapper to get the assets in the game. We need different classes
     // or components for each type of card to handle the specialized logic
     const key = PLAYER_CARD_INFO[type].key;
-    this.sprite = scene.add.sprite(x, y, "assets", `cards/${key}`).setOrigin(0, 0);
+    this.sprite = scene.add
+      .sprite(0, 0, "assets", `cards/${key}`)
+      .setOrigin(0.5, 0.5)
+      .setInteractive();
+
+    this.outline = scene.add.sprite(0, 0, "assets", `cards/outline`).setOrigin(0.5, 0.5);
+    this.outline.setAlpha(0);
+
+    this.setPosition(x, y);
+
+    this.selected = false;
+    this.sprite.on("pointerdown", () => (this.selected ? this.deselect() : this.select()));
   }
 
   getPosition() {
     return { x: this.sprite.x, y: this.sprite.y };
   }
 
+  select() {
+    this.selected = true;
+    this.scene.tweens.killTweensOf(this.outline);
+    this.scene.tweens.add({
+      targets: [this.sprite, this.outline],
+      scaleY: 1.1,
+      scaleX: 1.1,
+      duration: 200,
+      ease: "Quad.easeOut"
+    });
+    this.scene.tweens.add({
+      targets: this.outline,
+      alpha: 1,
+      duration: 200,
+      ease: "Quad.easeOut"
+    });
+  }
+
+  deselect() {
+    this.selected = false;
+    this.scene.tweens.killTweensOf(this.outline);
+    this.scene.tweens.add({
+      targets: [this.sprite, this.outline],
+      scaleY: 1,
+      scaleX: 1,
+      duration: 200,
+      ease: "Quad.easeOut"
+    });
+    this.scene.tweens.add({
+      targets: this.outline,
+      alpha: 0,
+      duration: 200,
+      ease: "Quad.easeOut"
+    });
+  }
+
   setPosition(x, y) {
-    this.sprite.x = x;
-    this.sprite.y = y;
+    const cx = x + this.sprite.width / 2;
+    const cy = y + this.sprite.height / 2;
+    this.sprite.x = cx;
+    this.sprite.y = cy;
+    this.outline.x = this.sprite.x;
+    this.outline.y = this.sprite.y;
   }
 
   moveTo() {
