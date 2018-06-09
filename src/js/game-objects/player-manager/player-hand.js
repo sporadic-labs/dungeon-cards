a94ba8d16ee1635d0430de8c0feeba5eb39d3404
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-09 15:36:11 -0500

    PlayerHand: track whether hand is selectable so that new cards that are drawn can be selected

--- src/js/game-objects/player-manager/player-hand.js
+++ src/js/game-objects/player-manager/player-hand.js
@@ -11,6 +11,7 @@ export default class PlayerHand {
     this.scene = scene;
     this.deck = deck;
     this.cards = [];
+    this.selectingEnabled = false;
 
     emitter.on(EVENT_NAMES.PLAYER_CARD_SELECT, card => {
       this.cards.forEach(c => c.deselect());
@@ -32,10 +33,12 @@ export default class PlayerHand {
   }
 
   enableSelecting() {
+    this.selectingEnabled = true;
     this.cards.forEach(c => c.enableSelecting());
   }
 
   disableSelecting() {
+    this.selectingEnabled = false;
     this.cards.forEach(c => c.deselect());
     this.cards.forEach(c => c.disableSelecting());
   }
@@ -48,7 +51,9 @@ export default class PlayerHand {
     if (!this.deck.anyCardsRemaining()) return;
     // Tell a card to animate from deck position to hand
     const cardId = this.deck.draw();
-    this.cards.push(new PlayerCard(this.scene, cardId, 0, 0));
+    const card = new PlayerCard(this.scene, cardId, 0, 0);
+    this.cards.push(card);
+    if (this.selectingEnabled) card.enableSelecting();
     this.arrangeCards();
   }
 
