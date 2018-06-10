40ec566646b3751f7e711cccd325e4ab122b1ffe
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-10 14:42:44 -0500

    Adding more fine-grained events for each round of the game

--- src/js/game-objects/game-runner/events.js
+++ src/js/game-objects/game-runner/events.js
@@ -2,7 +2,11 @@ import EmitterWithLogging from "../../helpers/emitter-with-logging";
 
 const EVENT_NAMES = {
   ROUND_START: "ROUND_START",
-  ROUND_END: "ROUND_END"
+  ROUND_END: "ROUND_END",
+  PLAYER_TURN_START: "PLAYER_TURN_START",
+  PLAYER_TURN_END: "PLAYER_TURN_END",
+  ENEMY_TURN_START: "ENEMY_TURN_START",
+  ENEMY_TURN_END: "ENEMY_TURN_END"
 };
 
 const emitter = new EmitterWithLogging("Game Manager");
--- src/js/game-objects/game-runner/run.js
+++ src/js/game-objects/game-runner/run.js
@@ -3,8 +3,13 @@ import { emitter, EVENT_NAMES } from "./events";
 export default async function run(playerManager, enemyManager) {
   emitter.emit(EVENT_NAMES.ROUND_START);
 
+  emitter.emit(EVENT_NAMES.ENEMY_TURN_START);
   await enemyManager.update();
+  emitter.emit(EVENT_NAMES.ENEMY_TURN_END);
+
+  emitter.emit(EVENT_NAMES.PLAYER_TURN_START);
   await playerManager.update();
+  emitter.emit(EVENT_NAMES.PLAYER_TURN_END);
 
   emitter.emit(EVENT_NAMES.ROUND_END);
 
