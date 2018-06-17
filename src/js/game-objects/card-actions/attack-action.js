91f131e2fbf44422edebb7c08ea34365bee454d8
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-16 22:29:46 -0500

    WIP: factor in the attack pattern into the attack action

--- src/js/game-objects/card-actions/attack-action.js
+++ src/js/game-objects/card-actions/attack-action.js
@@ -2,27 +2,51 @@ import { emitter, EVENT_NAMES } from "../game-runner";
 import logger from "../../helpers/logger";
 import { EventProxy } from "../events";
 
+function getAttackPositions(pointer, gameBoard, attackPattern) {
+  const positions = [];
+
+  // TODO: this is wrong, the center of the attack can be off the board
+  const boardPosition = gameBoard.getBoardPosition(pointer.x, pointer.y);
+  if (!boardPosition) return [];
+
+  attackPattern.forEach(({ x: dx, y: dy }) => {
+    const x = boardPosition.x + dx;
+    const y = boardPosition.y + dy;
+    if (gameBoard.isInBounds(x, y)) positions.push({ x, y });
+  });
+
+  return positions;
+}
+
 export default function attackAction(playerManager, card) {
   const { gameBoard, scene } = playerManager;
-
   const proxy = new EventProxy();
+  const attackPattern = card.cardInfo.cells;
 
   proxy.on(scene.input, "pointerover", pointer => {
-    const boardPosition = gameBoard.getBoardPosition(pointer.x, pointer.y);
-    if (boardPosition) {
-      logger.log(`You are hovering over (${boardPosition.x}, ${boardPosition.y}) on the board`);
-      emitter.emit(EVENT_NAMES.GAMEBOARD_CARD_FOCUS, card, boardPosition.x, boardPosition.y);
-    }
+    const positions = getAttackPositions(pointer, gameBoard, attackPattern);
+    positions.forEach(({ x, y }) => {
+      const enemy = gameBoard.getAt(x, y);
+      if (enemy) {
+        logger.log(`You are hovering over (${x}, ${y}) on the board`);
+        emitter.emit(EVENT_NAMES.GAMEBOARD_CARD_FOCUS, card, x, y);
+      }
+    });
   });
 
   proxy.on(scene.input, "pointerdown", pointer => {
-    const boardPosition = gameBoard.getBoardPosition(pointer.x, pointer.y);
-    if (boardPosition) {
-      logger.log(`You a ttacked at (${boardPosition.x}, ${boardPosition.y}) on the board`);
-      emitter.emit(EVENT_NAMES.ACTION_COMPLETE, card, boardPosition.x, boardPosition.y);
+    const positions = getAttackPositions(pointer, gameBoard, attackPattern);
+
+    if (positions.length) {
+      positions.forEach(({ x, y }) => {
+        const enemy = gameBoard.getAt(x, y);
+        if (enemy) {
+          logger.log(`You attacked at (${x}, ${y}) on the board`);
+          emitter.emit(EVENT_NAMES.ACTION_COMPLETE, card, x, y);
+        }
+      });
+
       proxy.removeAll();
-    } else {
-      logger.log("Invalid attack location!");
     }
   });
 }
