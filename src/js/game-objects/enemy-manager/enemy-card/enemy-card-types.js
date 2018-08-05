d39c5ae4d97daf4ea34e85db4a71bcf74033094a
rex <retwedt@gmail.com>
2018-08-04 19:18:32 -0500

    Add enemy info object, with key, description, and health.  Import where useful.

--- src/js/game-objects/enemy-manager/enemy-card/enemy-card-types.js
+++ src/js/game-objects/enemy-manager/enemy-card/enemy-card-types.js
@@ -1,7 +1,29 @@
-const ENEMY_CARD_TYPES = {
-  WEAK_ENEMY: 0,
-  STRONG_ENEMY: 1,
-  BLANK: 2
+const ENEMY_CARD_INFO = {
+  WEAK_ENEMY: {
+    key: "cards/card-contents-enemy-small",
+    description:
+      "This small enemy can move up to 1 square at a time. It is weak, having only 1 health.",
+    health: 1
+  },
+  STRONG_ENEMY: {
+    key: "cards/card-contents-enemy-big",
+    description:
+      "This larger enemy can move up to 1 square at a time. It is more dangerous, having 2 health.",
+    health: 2
+  },
+  BLANK: {
+    key: null,
+    description: "This blank card can do no harm.  Awesome!",
+    health: null
+  }
 };
 
-export default ENEMY_CARD_TYPES;
+/**
+ * @enum {ENEMY_CARD_TYPES}
+ * @readonly
+ * @export
+ */
+const ENEMY_CARD_TYPES = {};
+Object.keys(ENEMY_CARD_INFO).forEach(key => (ENEMY_CARD_TYPES[key] = key));
+
+export { ENEMY_CARD_INFO, ENEMY_CARD_TYPES };
--- src/js/game-objects/enemy-manager/enemy-card/index.js
+++ src/js/game-objects/enemy-manager/enemy-card/index.js
@@ -1,5 +1,5 @@
 import EnemyCard from "./enemy-card";
-import ENEMY_CARD_TYPES from "./enemy-card-types";
+import { ENEMY_CARD_INFO, ENEMY_CARD_TYPES } from "./enemy-card-types";
 
 export default EnemyCard;
-export { ENEMY_CARD_TYPES };
+export { ENEMY_CARD_INFO, ENEMY_CARD_TYPES };
