607f0ea5e00b08e4d9e467ea8d35b4089ae66791
Michael Hadley <michaelwesthadley@gmail.com>
2018-08-26 20:17:32 -0500

    Update event proxy to guard against listeners being invoked after unsubscribed

--- src/js/game-objects/events/event-proxy.js
+++ src/js/game-objects/events/event-proxy.js
@@ -1,5 +1,45 @@
 import logger from "../../helpers/logger";
-import { arrayEqual } from "../../helpers/array-utils";
+
+/**
+ * An event listener with a guard against being invoked after it has been unsubscribed. Note, we
+ * could speed this up by using the life cycle plugin pattern of proxying the emitter - rather than
+ * the individual listeners - by using a map from emitter => array of listeners.
+ *
+ * @class EventListener
+ */
+class EventListener {
+  constructor(eventEmitter, eventName, listener, context, isOnce = false) {
+    this.eventEmitter = eventEmitter;
+    this.eventName = eventName;
+    this.listener = listener;
+    this.context = context;
+    this.isSubscribed = true;
+
+    if (isOnce) eventEmitter.once(eventName, this.onEvent, this);
+    else eventEmitter.on(eventName, this.onEvent, this);
+  }
+
+  /**
+   * Does this listener match the given signature
+   */
+  doesMatch(eventEmitter, eventName, listener, context) {
+    return (
+      eventEmitter === this.eventEmitter &&
+      this.eventName === eventName &&
+      this.listener === listener &&
+      context === this.context
+    );
+  }
+
+  onEvent(...args) {
+    if (this.isSubscribed) this.listener.apply(this.context, args);
+  }
+
+  destroy() {
+    this.isSubscribed = false;
+    this.eventEmitter.off(this.eventName, this.onEvent, this);
+  }
+}
 
 /**
  * A proxy that tracks subscriptions to EventEmitters, so that all listeners can be unsubscribed
@@ -10,31 +50,30 @@ import { arrayEqual } from "../../helpers/array-utils";
  * @class EventProxy
  */
 export default class EventProxy {
-  /**
-   * Array of subscriptions to EventEmitters, in the form [emitter, event, listener, context].
-   *
-   * @memberof EventProxy
-   */
-  subscriptions = [];
+  listeners = [];
 
   on(eventEmitter, eventName, listener, context) {
-    this.subscriptions.push([eventEmitter, eventName, listener, context]);
-    eventEmitter.on(eventName, listener, context);
+    this.listeners.push(new EventListener(eventEmitter, eventName, listener, context));
   }
 
   once(eventEmitter, eventName, listener, context) {
-    this.subscriptions.push([eventEmitter, eventName, listener, context]);
-    eventEmitter.once(eventName, listener, context);
+    this.listeners.push(new EventListener(eventEmitter, eventName, listener, context), true);
   }
 
   off(eventEmitter, eventName, listener, context) {
-    const args = [eventEmitter, eventName, listener, context];
-    this.subscriptions = this.subscriptions.filter(array => !arrayEqual(array, args));
-    eventEmitter.off(eventName, listener, context);
+    this.listeners = this.listeners.filter(listener => {
+      if (listener.doesMatch(eventEmitter, eventName, listener, context)) {
+        listener.destroy();
+        return false;
+      }
+      return true;
+    });
   }
 
   debugDump() {
-    logger.log(`[${this.subscriptions.map(args => `\n\tEvent "${args[1]}"`).join(",")}\n]`);
+    logger.log(
+      `[${this.listeners.map(listener => `\n\tEvent "${listener.eventName}"`).join(",")}\n]`
+    );
   }
 
   /**
@@ -43,8 +82,7 @@ export default class EventProxy {
    * @memberof EventProxy
    */
   removeAll() {
-    this.subscriptions.forEach(([eventEmitter, eventName, listener, context]) =>
-      eventEmitter.off(eventName, listener, context)
-    );
+    this.listeners.forEach(listener => listener.destroy());
+    this.listeners = [];
   }
 }
