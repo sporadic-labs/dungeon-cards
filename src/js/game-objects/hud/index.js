4b49a9db05d0d724852b6819528c31b7926eaebb
rex <retwedt@gmail.com>
2018-10-13 14:26:40 -0500

    Don't need the PopUpText class anymore.

--- src/js/game-objects/hud/index.js
+++ src/js/game-objects/hud/index.js
@@ -1,7 +1,6 @@
 import EndTurnButton from "./end-turn-button";
 import DropTarget from "./drop-target";
 import EnergyDisplay from "./energy-display";
-import PopupText from "./pop-up-text";
 import DeckDisplay from "./deck-display";
 
-export { DeckDisplay, DropTarget, EndTurnButton, EnergyDisplay, PopupText };
+export { DeckDisplay, DropTarget, EndTurnButton, EnergyDisplay };
