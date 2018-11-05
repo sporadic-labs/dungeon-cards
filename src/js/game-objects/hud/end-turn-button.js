afbd48b9722a2f356df88d8a016f48a85adf2320
Michael Hadley <michaelwesthadley@gmail.com>
2018-11-04 20:08:46 -0600

    Refactor EndTurnButton to use new Button class

--- src/js/game-objects/hud/end-turn-button.js
+++ src/js/game-objects/hud/end-turn-button.js
@@ -1,4 +1,5 @@
-import { emitter, EVENT_NAMES } from "../events";
+import { emitter, EVENT_NAMES, EventProxy } from "../events";
+import Button from "./button";
 
 /**
  * @export
@@ -11,50 +12,32 @@ export default class EndTurnButton {
    * @param {number} x
    * @param {number} y
    */
-  constructor(scene, x, y) {
+  constructor(scene, x, y, origin) {
     this.scene = scene;
 
-    this.sprite = scene.add
-      .sprite(0, 0, "assets", "ui/end-turn-normal")
-      .setOrigin(0.5, 0.5)
-      .setInteractive();
+    this.button = new Button(scene, x, y, {
+      framePrefix: "ui/end-turn-",
+      origin,
+      onDown: () => emitter.emit(EVENT_NAMES.PLAYER_TURN_ATTEMPT_COMPLETE)
+    });
 
-    this.sprite.setPosition(x, y);
-  }
-
-  enableInteractivity() {
-    this.sprite.setTexture("assets", "ui/end-turn-normal");
-    this.sprite.on("pointerdown", this.onPointerDown, this);
-    this.sprite.on("pointerover", this.onPointerOver, this);
-    this.sprite.on("pointerout", this.onPointerOut, this);
-  }
-
-  disableInteractivity() {
-    this.sprite.setTexture("assets", "ui/end-turn-normal");
-    this.sprite.off("pointerdown", this.onPointerDown, this);
-    this.sprite.off("pointerover", this.onPointerOver, this);
-    this.sprite.off("pointerout", this.onPointerOut, this);
-  }
-
-  onPointerDown() {
-    emitter.emit(EVENT_NAMES.PLAYER_TURN_ATTEMPT_COMPLETE);
-  }
-
-  onPointerOver() {
-    this.sprite.setTexture("assets", "ui/end-turn-pressed");
-  }
-
-  onPointerOut() {
-    this.sprite.setTexture("assets", "ui/end-turn-normal");
+    this.proxy = new EventProxy();
+    this.proxy.on(scene.events, "shutdown", this.destroy, this);
+    this.proxy.on(scene.events, "destroy", this.destroy, this);
   }
 
   activate() {
-    this.sprite.setAlpha(1);
-    this.enableInteractivity();
+    this.button.sprite.setAlpha(1);
+    this.button.enableInteractivity();
   }
 
   deactivate() {
-    this.sprite.setAlpha(0.8);
-    this.disableInteractivity();
+    this.button.sprite.setAlpha(0.8);
+    this.button.disableInteractivity();
+  }
+
+  destroy() {
+    this.button.destroy();
+    this.proxy.removeAll();
   }
 }
