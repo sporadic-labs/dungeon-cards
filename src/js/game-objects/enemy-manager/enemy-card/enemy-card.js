7f9b0a334ab8c00d6e2a1f3f5fd324088df009ab
Michael Hadley <michaelwesthadley@gmail.com>
2018-11-11 15:17:57 -0600

    Remove dead code

--- src/js/game-objects/enemy-manager/enemy-card/enemy-card.js
+++ src/js/game-objects/enemy-manager/enemy-card/enemy-card.js
@@ -139,11 +139,7 @@ export default class EnemyCard {
   focus() {
     if (this.focused) return;
     this.focused = true;
-
     this.scene.tweens.killTweensOf(this.cardFront);
-    // this.cardEmitter.emit("pointerover", this);
-
-    // this.scene.tweens.killTweensOf(this.container);
     return new Promise(resolve => {
       this.scene.tweens.add({
         targets: this.cardFront,
@@ -159,11 +155,7 @@ export default class EnemyCard {
   defocus() {
     if (!this.focused) return;
     this.focused = false;
-
     this.scene.tweens.killTweensOf(this.cardFront);
-    // this.cardEmitter.emit("pointerout", this);
-
-    // this.scene.tweens.killTweensOf(this.container);
     return new Promise(resolve => {
       this.scene.tweens.add({
         targets: this.cardFront,
