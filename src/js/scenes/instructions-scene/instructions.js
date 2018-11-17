d65272b47ff101c18740996000fa8f8c7f1bf25d
Michael Hadley <michaelwesthadley@gmail.com>
2018-11-17 16:17:32 -0600

    Add title to each step of instructions

--- src/js/scenes/instructions-scene/instructions.js
+++ src/js/scenes/instructions-scene/instructions.js
@@ -1,21 +1,26 @@
 const instructions = [
   {
+    title: "Your Hand",
     text:
       "These are your cards - your actions and attacks that you can play during your turn. You draw a new one at the start of your turn. The number in the top right of each card is the energy you need to play that card."
   },
   {
+    title: "Your Energy",
     text:
       "This is how much energy you currently have. You can play cards during your turn to gain energy. Your energy resets to zero at the end of your turn."
   },
   {
+    title: "Gaining Energy",
     text:
       "This area is where you can play cards to gain energy. You can either play energy cards, or discard certain attack cards to reclaim them as energy."
   },
   {
+    title: "Advancing Enemies",
     text:
       "These enemy cards will start at the top of the grid and advance towards you on their turn. If any reach the last row, you’ll lose."
   },
   {
+    title: "Clearing the Dungeon",
     text:
       "This deck contains all the cards in this dungeon. When you defeat all the enemies and there are no cards left in this deck, you’ve cleared this dungeon and won the game."
   }
